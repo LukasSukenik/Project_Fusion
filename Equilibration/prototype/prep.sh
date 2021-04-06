2816000d5b22c66754a8885479edd09487205bd5
Lukas Sukenik <sukeniklukas@gmail.com>
2021-04-06 17:42:08 +0200

    Wall update

--- Equilibration/prototype/prep.sh
+++ Equilibration/prototype/prep.sh
@@ -7,12 +7,14 @@ function gen_in
   r_run=$3
   r_damp=100.0
   r_timestep=0.01
+  r_radius=$(($4-3))
 
   cp $1 $2
   sed -i -e 's/r_damp/'"$r_damp"'/g' $2
   sed -i -e 's/r_random/'"$RANDOM"'/g' $2
   sed -i -e 's/r_timestep/'"$r_timestep"'/g' $2
   sed -i -e 's/r_run/'"$r_run"'/g' $2
+  sed -i -e 's/r_radius/'"$r_radius"'/g' $2
 }
 
 function gen_ves
@@ -27,8 +29,6 @@ function gen_ves
   sed -i -e 's/sed_radius/'"$radius"'/g' $2
 }
 
-gen_in "in.prescript_equi" "in.equi" $1
+gen_in "in.prescript_equi" "in.equi" $1 $4
 gen_ves prescript_vesicle vesicle $2 $3 $4
 ./gen_membrane vesicle
-
-
