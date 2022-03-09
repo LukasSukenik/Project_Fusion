0814df7834047962accc78da595fe036726c3e57
Sofia Blasco <sofbp@sof.ceitec.muni.cz>
2022-03-09 11:07:42 +0100

    added head size

--- Nanoparticle_equilibration/prepare.sh
+++ Nanoparticle_equilibration/prepare.sh
@@ -9,8 +9,9 @@ inter=$3
 prol=$4
 sca=$5
 boxx=$6
+head=$7
 
-folder="r${rad}_i${inter}_c${prol}_s${sca}"
+folder="r${rad}_h${head}_i${inter}_c${prol}_s${sca}"
 
 echo $folder
 
@@ -19,21 +20,23 @@ mkdir $folder
 cp prototype/* $folder/
 
 # Copy endfile (data.end) from vesicle equilibration and rename to data.equi
+cp ../Equilibration/r${rad}\_h${head}\_*/data.end $folder/data.equi
 
 cd $folder
-  ./prep.sh $run ${inter} ${prol} ${sca} ${boxx}
+  ./prep.sh $run ${inter} ${prol} ${sca} ${boxx} ${head}
 cd ..
 
 }
 
-r_run=250000 
+r_run=500000 
 radius=10
-interaction=3
-scale=4
+interaction=2
 prolatenes=3 # CANT use 1.0 -> 1.001
+scale=3
 box=30
+head_size=0.95
 
-create $r_run $radius $interaction $prolatenes $scale $box
+create $r_run $radius $interaction $prolatenes $scale $box $head_size
 
 
 
